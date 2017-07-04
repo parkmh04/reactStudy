a260407de0d456aac3b8559a68d78ac7ae5e1e58
황진하 <gojinha@naver.com>
2017-07-04 13:59:28 +0900

    tab length 4 to 2 update

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,57 +4,59 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
-    entry: [
-        './src/index.jsx',
-        'webpack-dev-server/client?http://0.0.0.0:3001',
-        'webpack/hot/only-dev-server'
-    ],
+	entry: [
+		'./src/index.jsx', 'webpack-dev-server/client?http://0.0.0.0:3001', 'webpack/hot/only-dev-server'
+	],
 
-    output: {
-        path: '/',
-        filename: 'bundle.js'
-    },
+	output: {
+		path: '/',
+		filename: 'bundle.js'
+	},
 
-    devtool: 'eval-source-map',
+	devtool: 'eval-source-map',
 
-    devServer: {
-        hot: true,
-        filename: 'bundle.js',
-        publicPath: '/',
-        historyApiFallback: true,
-        contentBase: path.join(__dirname, '/dist/'),
-        proxy: {
-            "*": "http://localhost:3000"
-        }
-    },
+	devServer: {
+		hot: true,
+		filename: 'bundle.js',
+		publicPath: '/',
+		historyApiFallback: true,
+		contentBase: path.join(__dirname, '/dist/'),
+		proxy: {
+			'*': 'http://localhost:3000'
+		}
+	},
 
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            include: path.join(__dirname, 'src'),
-            use: [{
-                loader: 'react-hot-loader'
-            }, {
-                loader: 'babel-loader'
-            }]
-        }, {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-        }]
-    },
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				include: path.join(__dirname, 'src'),
+				use: [
+					{
+						loader: 'react-hot-loader'
+					}, {
+						loader: 'babel-loader'
+					}
+				]
+			}, {
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			}
+		]
+	},
 
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            minify: {
-                collapseWhitespace: true,
-                keepClosingSlash: true,
-                removeComments: true
-            }
-        }),
-    ]
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NamedModulesPlugin(),
+		new webpack.NoEmitOnErrorsPlugin(),
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			minify: {
+				collapseWhitespace: true,
+				keepClosingSlash: true,
+				removeComments: true
+			}
+		})
+	]
 };
