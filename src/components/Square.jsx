27e45addfb8a05615166450cc641805afee38784
황진하 <gojinha@naver.com>
2017-07-13 10:13:54 +0900

    Squre render performance tuned

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Square(props) {
-	return (
-		<button className="square" onClick={() => props.onClick()}>
-			{props.value}
-		</button>
-	);
+class Square extends Component {
+	constructor(props) {
+		super(props);
+
+	}
+	shouldComponentUpdate(nextProps) {
+
+		return (JSON.stringify(nextProps) != JSON.stringify(this.props));
+	}
+	render() {
+		console.log(`render: ${this.props.seq}`);
+		return (
+			<button className="square" onClick={() => this.props.onClick()}>
+				{this.props.value}
+			</button>
+		);
+	}
+
 }
+
 export default Square;
